d48342fc3f0a40e42c7db11bb453f3e055e33862
Leonardo de Pinho <pinholeonardo1777@gmail.com>
2024-07-15 12:26:19 -0300

    video at 29min

--- src/WeeklyCalendar.js
+++ src/WeeklyCalendar.js
@@ -9,13 +9,26 @@ const HOUR_MARGIN_TOP = 15;
 
 export const WeeklyCalendar = () => {
     const [mondayDate, setMondayDate] = useState(getMonday());
+    const [tasks, setTasks] = useState([
+        { date: new Date(2024, 6, 16, 10), text: "first hi", howLong: 3},
+        { date: new Date(2024, 6, 17, 11), text: "second", howLong: 2} ,
+        { date: new Date(2024, 7, 10, 16), text: "birthday", howLong: 8}
+    ])
 
     const hourNow= new Date().getHours();
     const minutesNow = new Date().getMinutes();
     
+    const nextWeek = () => setMondayDate(addDateBy(mondayDate, 7));
+    const prevWeek = () => setMondayDate(addDateBy(mondayDate, -7));
 
     return (<>
-    
+    <FlexBox>
+        <p>Today: {new Date().toDateString()}</p>
+        <p>From: {mondayDate.toDateString()}</p>
+        <p>To: {addDateBy(mondayDate, 6).toDateString()}</p>
+        <button onClick={prevWeek}><ion-icon name="arrow-back-outline"></ion-icon></button>
+        <button onClick={nextWeek}><ion-icon name="arrow-forward-outline"></ion-icon></button>
+    </FlexBox>
     <Wrapper>
         <HGrid first={'30px'} cols={1}>
             <VGrid rows={24}>
@@ -29,13 +42,27 @@ export const WeeklyCalendar = () => {
                         <DayWrapper isToday={
                             areDatesSame(new Date(), addDateBy(mondayDate, index))}>
                             <p>{day}</p>
+                            {tasks.map((task) => 
+                                (areDatesSame( addDateBy(mondayDate, index), task.date) && 
+                                (<Event
+                                howLong={task.howLong}
+                                fromTop={
+                                    task.date.getHours() * HOUR_HEIGHT + 
+                                    HOUR_MARGIN_TOP + 
+                                    HOUR_HEIGHT / 2 + 
+                                    task.date.getMinutes() / 2}
+                                >Hi</Event>))
+                            )}
                         </DayWrapper>
                 ))
                 }
             </HGrid>
         </HGrid>
         <HourLine fromTop={
-            hourNow * HOUR_HEIGHT + HOUR_MARGIN_TOP + HOUR_HEIGHT / 2 + minutesNow / 2
+            hourNow * HOUR_HEIGHT + 
+            HOUR_MARGIN_TOP + 
+            HOUR_HEIGHT / 2 + 
+            minutesNow / 2
         }/>
     </Wrapper></>)
 }
@@ -60,6 +87,8 @@ grid-template-rows: repeat(${({ rows }) => rows}, 1fr);
 
 const DayWrapper = styled.span`
 border: 1px solid red;
+display: relative;
+background: ${({isToday}) => isToday ? '#F2CEE6' : ''}
 
 `
 
@@ -78,4 +107,30 @@ position: absolute;
 width: 100%;
 top: ${({fromTop}) => fromTop}px;
 border: 1px solid orange;
+`
+
+const FlexBox = styled.div`
+display: flex;
+justify-content: space-around;
+front-size: 1.2rem;
+margin-top: 20px;
+
+button {
+font-size: 1.2rem;
+display: flex;
+align-items: center;
+}
+
+`
+
+const Event = styled.div`
+position: relative;
+top: ${({fromTop}) => fromTop}px;
+background: green;
+height: ${({howLong}) => howLong * HOUR_HEIGHT}px;
+color: white;
+// width: calc(100% - 10px);
+margin: 0 5px;
+padding: 5px;
+border-radius: 6px;
 `
--- src/utils.js
+++ src/utils.js
@@ -9,12 +9,13 @@ export const areDatesSame = (first, second) => {
 export const addDateBy = (date, count) => {
     const d = new Date(date);
     return new Date(d.setDate(d.getDate() + count))
-    
+    //d é um novo objeto de Date com a data passa nos argumentos.
+    //o dia atual de d é obtido e adiciona-se count à ele.
+    //O dia atual somado ao count é atribuído ao próprio d, mudando assim seu dia com base no seu dia original
 }
 
 export const getMonday = () => {
-    const today = new Date();
-    
-    const first = today.getDate() - today.getDay + 1;
-    return new Date(today.setDate(first))
+    const today = new Date(); //sets today to present moment   
+    const first = today.getDate() - today.getDay() + 1// dia de hoje - dia da semana de hoje + 1 (a segunda feira é o dia 1, então se o desejo fosse encontrar o domingo, seria usado o 0)
+    return new Date(today.setDate(first))//retorna a data da segunda feira da semana atual
 }
